1479140c070318b908c4e54195f6aa86f4160be2
Lucas. <lucascamposnew@outlook.com.>
2026-05-07 12:09:08 -0300

    Adicionei o pacote configuration, domain, services e fiz os endpoints das requisições

---
+++ first-spring-app/src/main/java/com/infinity/first_spring_app/configuration/HelloConfiguration.java
@@ -0,0 +1,9 @@
+package com.infinity.first_spring_app.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloConfiguration {
+
+
+}
--- first-spring-app/src/main/java/com/infinity/first_spring_app/controller/HelloWorldController.java
+++ first-spring-app/src/main/java/com/infinity/first_spring_app/controller/HelloWorldController.java
@@ -1,16 +1,30 @@
 package com.infinity.first_spring_app.controller;
 
+import com.infinity.first_spring_app.domain.User;
+import com.infinity.first_spring_app.services.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
+    @Autowired
+    private HelloWorldService helloWorldService;
+
+    public HelloWorldController(HelloWorldService helloWorldService) {
+
+        this.helloWorldService =   helloWorldService;
+    }
+
     @GetMapping
     public String helloWorld() {
-        return "Hello World!";
+        return helloWorldService.helloWorld("Lucas");
+    }
+
+    @PostMapping
+    public String helloWorldPost(@RequestBody User body) {
+        return "Hello World + " + body.getName();
     }
 }
